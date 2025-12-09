1cf39bd4cce98eef3b6eb97610843c225d600672
Arman Shaikh <armaanshakeelshaikh@gmail.com>
2025-12-09 15:57:45 +0400

    Fixed setup.py for launch files

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'autonomous_sphere_collector_pkg'
 
@@ -10,6 +12,21 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+         # Install launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+
+        # Install URDF/Xacro files
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+
+        # Install config files
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+
+        # Install RViz configs
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
+
+        # Install resource files (worlds, etc.)
+        (os.path.join('share', package_name, 'resource'), glob('resource/*')),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
